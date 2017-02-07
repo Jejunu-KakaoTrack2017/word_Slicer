8d2ad18adb78612dc4fa2989b9d118d37c078926
Jaehyeon Kim <replaykim123@gmail.com>
2017-02-07 16:14:24 +0900

    배열대신 리스트사용으로 리팩토링

--- src/main/java/WordSlicer.java
+++ src/main/java/WordSlicer.java
@@ -1,18 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by replay on 2017. 2. 4..
  */
 public class WordSlicer {
 
 
-    public String[] slicer(String dummy){
+    public List<String> slicer(String dummy){
         //마침표나 콤마를 지운다.
         dummy = dummy.replaceAll(",","").replaceAll("\\.","");
 
+        //삭제를 용이하게 할수있게 리스트 사용
         //띄어쓰기를 기준으로 자른다.
-        String[] data = dummy.split(" ");
+        List<String> data = new ArrayList<String>();
+        Collections.addAll(data, dummy.split(" "));
 
-        for (int i = 0; i <data.length; i++){
-            System.out.println(data[i]);
+        //한 글자인 것을 삭제
+        for (int i = 0; i <data.size(); i++){
+            if (data.get(i).length() <= 1){
+                data.remove(i);
+            }
+            else{
+
+            }
+
+        }
+
+
+        // 테스트용 출력ㄱ
+        for (int i = 0; i <data.size(); i++){
+            System.out.println(data.get(i));
         }
 
         return data;
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -8,10 +8,12 @@ public class test {
     @org.junit.Test
     public void test_문장을_단어단위로자른다(){
         String dummy = "Lead with your strengths, not your weaknesses.";
+        String dummy2 = "In times of change, learners inherit the earth, while the learned find themselves beautifully equipped to deal with a world that no longer exists.";
+
 
         WordSlicer wordSlicer = new WordSlicer();
 
-        Assert.assertNotNull(wordSlicer.slicer(dummy));
+        Assert.assertNotNull(wordSlicer.slicer(dummy2));
 
     }
 }
